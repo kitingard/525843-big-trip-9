b55a5e614ab52f698f242fe831fb745beb607ef3
Ekaterina Matyushkina <yvpetr56@mts.ru>
2019-08-26 18:07:50 +0300

    Добавлены новые свойства в компонент data

--- src/components/data.js
+++ src/components/data.js
@@ -1,10 +1,37 @@
 const getRandom = (count) => Math.floor(Math.random() * count);
 
 export const cardData = () => ({
-  eventType: [`bus`, `check-in`, `drive`, `flight`, `restaurant`, `ship`, `sightseeing`, `taxi`, `train`, `transport`, `trip`][getRandom(11)],
-  eventDate: Date.now() + 1 + getRandom(7) * 24 * 60 * 60 * 1000,
-
-  price: [10, 20, 50, 70, 100, 150, 200, 300][getRandom(8)],
-  city: [`Sankt-Peterburg`, `Smolensk`, `Moscow`, `Saratov`, `Sochi`][getRandom(5)],
-  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`.`).sort(() => Math.random() - 0.5).slice(0, getRandom(2)),
+  eventsDate: Date.now() + 1 + getRandom(2) * 24 * 60 * 60 * 1000,
+  event: {
+    type: [`bus`, `check-in`, `drive`, `flight`, `restaurant`, `ship`, `sightseeing`, `taxi`, `train`, `transport`, `trip`][getRandom(11)],
+    price: [10, 20, 50, 70, 100, 150, 200, 300][getRandom(8)],
+    city: [`Sankt-Peterburg`, `Smolensk`, `Moscow`, `Saratov`, `Sochi`][getRandom(5)],
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`.`).sort(() => Math.random() - 0.5).slice(0, getRandom(2)),
+    getDescription() {
+      switch (this.type) {
+        case `bus`:
+          return `Bus to ${this.city}`;
+        case `check-in`:
+          return `Check into hotel`;
+        case `drive`:
+          return `Drive to ${this.city}`;
+        case `flight`:
+          return `Flight to ${this.city}`;
+        case `restaurant`:
+          return `Dinner in ${this.city}`;
+        case `ship`:
+          return `Shopping in ${this.city}`;
+        case `sightseeing`:
+          return `Natural History Museum in ${this.city}`;
+        case `taxi`:
+          return `Taxi to airport`;
+        case `train`:
+          return `Train to ${this.city}`;
+        case `transport`:
+          return `Transport to ${this.city}`;
+        case `trip`:
+          return `Trip to ${this.city}`;
+      }
+    }
+  }
 });
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import {route} from "./components/route";
 import {menu} from "./components/menu";
 import {filter} from "./components/filter";
 import {sort} from "./components/sort";
+import {cardData} from "./components/data";
 import {card} from "./components/card";
 import {cardForm} from "./components/cardForm";
+
 
 const ELEMENTS_COUNT = 1;
 const CARDS_COUNT = 3;
@@ -13,16 +15,22 @@ const menuContainer = document.querySelector(`.trip-controls h2`);
 const filterContainer = document.querySelector(`.trip-controls`);
 const sortContainer = document.querySelector(`.trip-events h2`);
 
-const renderElement = (container, position, str, count) => {
-  container.insertAdjacentHTML(position, new Array(count).fill(str()).join(``));
+const cardsArray = new Array(CARDS_COUNT).fill(``).map(cardData);
+
+const renderElement = (container, position, str) => {
+  container.insertAdjacentHTML(position, new Array(ELEMENTS_COUNT).fill(str()).join(``));
 };
 
-renderElement(routeContainer, `afterbegin`, route, ELEMENTS_COUNT);
-renderElement(menuContainer, `afterend`, menu, ELEMENTS_COUNT);
-renderElement(filterContainer, `beforeend`, filter, ELEMENTS_COUNT);
-renderElement(sortContainer, `afterend`, sort, ELEMENTS_COUNT);
+const renderCard = (container, element) => {
+  container.insertAdjacentHTML(`beforeend`, cardsArray.map(element).join(``));
+};
+
+renderElement(routeContainer, `afterbegin`, route);
+renderElement(menuContainer, `afterend`, menu);
+renderElement(filterContainer, `beforeend`, filter);
+renderElement(sortContainer, `afterend`, sort);
 
 const cardsContainer = document.querySelector(`.trip-days`);
 
-renderElement(cardsContainer, `beforebegin`, cardForm, ELEMENTS_COUNT);
-renderElement(cardsContainer, `beforeend`, card, CARDS_COUNT);
+renderElement(cardsContainer, `beforebegin`, cardForm);
+renderCard(cardsContainer, card);
